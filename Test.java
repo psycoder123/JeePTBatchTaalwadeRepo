ea39bb3bb65505d0a862017eaabfc457ea2d23b0
rohit <rohit@gmail.com>
2017-10-27 15:23:19 +0530

    Ho gaya kam Test ka

---
+++ Test.java
@@ -0,0 +1,21 @@
+
+/**
+ * Hello world!
+ *
+ */
+public class Test
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+	public String greetMe()
+	{
+	return "Welcome Birdar";
+	}
+	public int add (int a,int b)
+	{
+	System.out.println("Addition:"+(a+b));
+	return a+b;
+	}
+}
